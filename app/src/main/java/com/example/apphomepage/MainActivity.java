0bc5cb92e05d28249752f779265850021d055662
Jainish5658 <jainish5658@gmail.com>
2022-12-23 21:21:51 +0530

    mentor mentee trial 1

--- app/src/main/java/com/example/apphomepage/MainActivity.java
+++ app/src/main/java/com/example/apphomepage/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.apphomepage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+     public CardView noticeboard;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       noticeboard = (CardView)  findViewById(R.id.noticeboard);
+
+       noticeboard.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+
+        switch (view.getId()){
+            case R.id.noticeboard :
+                i = new Intent(this,MentorMentee.class);
+                startActivity(i);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/apphomepage/MentorMentee.java
@@ -0,0 +1,13 @@
+package com.example.apphomepage;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MentorMentee extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mentor_mentee);
+    }
+}
