7aa5f058d2661a8dd196e7133be1863633c85aa0
Jainish5658 <jainish5658@gmail.com>
2022-12-24 13:17:58 +0530

    integrating slider k problem need to sort that problem

--- app/src/main/java/com/example/apphomepage/MainActivity.java
+++ app/src/main/java/com/example/apphomepage/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.apphomepage;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+
      public CardView noticeboard;
+
 
 
     @Override
@@ -17,10 +22,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
        noticeboard = (CardView)  findViewById(R.id.noticeboard);
 
        noticeboard.setOnClickListener(this);
     }
+
+
+
+
+
+
 
 
     @Override
